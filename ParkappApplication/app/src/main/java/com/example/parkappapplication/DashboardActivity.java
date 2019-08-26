304e369d6d8b57db9db52ab0427e2eb0ed1363ab
pv08 <pvbr08.tr@gmail.com>
2019-08-26 15:45:35 -0300

    Troca de dash

--- ParkappApplication/app/src/main/java/com/example/parkappapplication/DashboardActivity.java
+++ ParkappApplication/app/src/main/java/com/example/parkappapplication/DashboardActivity.java
@@ -1,31 +1,22 @@
 package com.example.parkappapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DashboardActivity extends AppCompatActivity {
+    private CardView mCardView;
+    DatabaseReference database = FirebaseDatabase.getInstance().getReference("Spots");
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
